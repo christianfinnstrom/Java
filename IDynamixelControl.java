60ce65a4531071a1acde761c71cc2d3ed4ce99bc
sigbauk <sigbauk@gmail.com>
2014-09-24 18:29:44 +0200

    added javadoc

--- IDynamixelControl.java
+++ IDynamixelControl.java
@@ -23,22 +23,49 @@ public interface IDynamixelControl extends Library{
 *	communication devices.
 */
 	
+	/**
+	 * Attempts to initialize the communication devices
+	 * @param devIndex Number of connected communication devices
+	 * @param baudnum 
+	 * @return 1 if success, 0 if failure
+	 */
 	int dxl_initialize(int devIndex, int baudnum);
 	
+	/**
+	 * Terminates the communication devices
+	 */
 	void dxl_terminate();
 	
 
+	
+	
 /*
 *	PACKET COMMUNICATION METHODS - subroutines used to transmit
 *	and receive packets.
 */
 	
+	/**
+	 *	Transmits instruction packet to Dynamixel 
+	 */
 	void dxl_tx_packet();
 	
+	/**
+	 *	Extracts status packet from the driver buffer
+	 */
 	void dxl_rx_packet();
 	
+	
+	/**
+	 * Runs dxl_tx_packet (transmits an instruction packet to the Dynamixel).
+	 * If success, run dxl_rx_packet (status packet is extracted from driver buffer).
+	 */
 	void dxl_txrx_packet();
 	
+	
+	/**
+	 * Get the current communication status
+	 * @return communication status identifier
+	 */
 	int dxl_get_result();
 
 
@@ -49,10 +76,23 @@ public interface IDynamixelControl extends Library{
 *	SET/GET PACKET METHODS - subroutines to make and 
 *	see the packets.
 */
+	/**
+	 * Sets the instruction packet ID
+	 * @param id Dynamixel ID to transmit instruction packet to
+	 */
     public void dxl_set_txpacket_id(int id);
     
+    /**
+     * Sets the instruction of the instruction packet
+     * @param instruction See Control table
+     */
     public void dxl_set_txpacket_instruction(int instruction);
 
+    /**
+     * Sets the parameter of the instruction packet
+     * @param index 
+     * @param value
+     */
     public void dxl_set_txpacket_parameter(int index, int value);
 
     public void dxl_set_txpacket_length(int length);
@@ -70,9 +110,10 @@ public interface IDynamixelControl extends Library{
 *	frequently used communication packets for user convenience.
 */
     
-    
+   
     /**
-     * @param id 
+     * @method dxl_ping Ping a Dynamixel to evaluate its existence
+     * @param id Dynamixel actuator ID
      */
     public void dxl_ping(int id);
     
@@ -85,7 +126,6 @@ public interface IDynamixelControl extends Library{
     public void dxl_write_word(int id, int address, int value);
     
     
-
 /*
 *	UTILITY METHODS - other useful subroutines
 */
