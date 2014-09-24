b1f8b80ee84e460f9cdee6cc2df97d67d964a717
christianfinnstrom <ca.finnstrom@gmail.com>
2014-09-24 18:52:26 +0200

    Added remaining javadoc

--- IDynamixelControl.java
+++ IDynamixelControl.java
@@ -90,17 +90,35 @@ public interface IDynamixelControl extends Library{
 
     /**
      * Sets the parameter of the instruction packet
-     * @param index 
-     * @param value
+     * @param index The parameter number to be set
+     * @param value The instruction value. See control table
      */
     public void dxl_set_txpacket_parameter(int index, int value);
 
+    /**
+     * Sets the instruction packet length
+     * @param length Length of instruction packet
+     */
     public void dxl_set_txpacket_length(int length);
 
+    /**
+     * Checks whether the status packet error equals the inputted error
+     * @param Bit Flag to check whether errors occur or not
+     * @return 1 if true, 0 if false
+     */
     public int dxl_get_rxpacket_error(int errbit);
 
+    /**
+     * Get length of status packet.
+     * @return Length of status packet
+     */
     public int dxl_get_rxpacket_length();
 
+    /**
+     * Gets the parameter value of the status packet
+     * @param is the parameter number to be set
+     * @return The parameter value at the "index"th element of status packet
+     */
     public int dxl_get_rxpacket_parameter(int index);
     
     
@@ -112,27 +130,65 @@ public interface IDynamixelControl extends Library{
     
    
     /**
-     * @method dxl_ping Ping a Dynamixel to evaluate its existence
+     * Ping a Dynamixel to evaluate its existence
      * @param id Dynamixel actuator ID
      */
     public void dxl_ping(int id);
     
+    /**
+     * Read one byte from the Dynamixel actuator
+     * @param id Dynamixel ID
+     * @return The read data value
+     */
     public int dxl_read_byte(int id, int address);
-
+    
+    /**
+     * Write one byte to the Dynamixel actuator
+     * @param id Dynamixel ID
+     * @param address Location of data to operate on (see Control Table)
+     * @param value Value to write on the Dynamixel at the desired address
+     */
     public void dxl_write_byte(int id, int address, int value);
 
+    /**
+     * Two bytes can be read from the Dynamixel actuator
+     * @param id Dynamixel ID
+     * @param address Location of data to operate on (see Control Table)
+     * @return The read data value
+     */
     public int dxl_read_word(int id, int address);
 
+    /**
+     * Two bytes can be written to the Dynamixel actuator
+     * @param id Dynamixel ID
+     * @param address Location of data to operate on (see Control Table)
+     * @param value The value to write on the Dynamixel
+     */
     public void dxl_write_word(int id, int address, int value);
     
     
 /*
 *	UTILITY METHODS - other useful subroutines
 */
+    /**
+     * Combines a lowbyte and highbyte and returns a word
+     * @param lowbyte Lower byte to be made of WORD-type
+     * @param highbyte Higher byte to be made of WORD-type
+     */
     public int dxl_makeword(int lowbyte, int highbyte);	
     
+    /**
+     * Returns the lowbyte of a word
+     * @param word Word-type data to extract lower byte from
+     * @return The lower byte extracted from word
+     */
     public int dxl_get_lowbyte(int word);
-
+    
+    /**
+     * Returns the highbyte of a word 
+     * @param word Word-type data to extract higher byte from
+     * @return The higher byte extracted from word
+     */
     public int dxl_get_highbyte(int word);
 
 }
