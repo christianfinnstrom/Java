f3cd4fc3127560e946677eaadfdd29a3a16ffaa1
sigbauk <sigbauk@gmail.com>
2014-10-21 16:53:45 +0200

    Små endringer
    endret navn på dictionary

--- Workspace/DynamixelControl/src/dxl_control/Test.java
+++ Workspace/DynamixelControl/src/dxl_control/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	
 	public static void main(String args[]){
 		
-		
+		/*
 		Actuator a = new Actuator(2);
 		
 		Communication.initialize();
@@ -22,6 +22,9 @@ public class Test {
 		
 		
 		Communication.terminate();
+		*/
+		
+		
 		
 	}
 
